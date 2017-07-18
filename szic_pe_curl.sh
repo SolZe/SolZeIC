01e9bb006bc49855124ab7cee90823b1b9ef66b5
Solinu <szabo.zsolo.laszlo@gmail.com>
2017-07-18 10:43:22 +0200

    Telepítési URL-ek frissítve
    Telepítési URL-ek frissítve
     - Wine és LAMP letöltési URL-ek frissítve.

--- szic_pe_curl.sh
+++ szic_pe_curl.sh
@@ -9,9 +9,9 @@ echo
 echo "Szoftverek és betűjeleik listája: https://github.com/Winizsol/telepitesi-scriptek/blob/master/SZOFTVEREK.md"
 read -p "Melyik szoftvert szeretné telepíteni? (betűjel) " choice
 case "$choice" in 
-  a|A ) cd ~/winizsol && curl -o winehq -L https://raw.githubusercontent.com/Winizsol/telepitesi-scriptek/master/wine.sh && chmod +x winehq && ./winehq && chmod -x winehq;;
+  a|A ) cd ~/winizsol && curl -o winehq -L https://raw.githubusercontent.com/SolZe/SolZeIC/master/wine.sh && chmod +x winehq && ./winehq && chmod -x winehq;;
   # Ez még nem működik
-  #b|B ) cd ~/winizsol && curl -o lamp -L https://raw.githubusercontent.com/Winizsol/telepitesi-scriptek/master/lamp.sh && chmod +x lamp && ./lamp && chmod -x lamp;;
+  #b|B ) cd ~/winizsol && curl -o lamp -L https://raw.githubusercontent.com/SolZe/SolZeIC/master/lamp.sh && chmod +x lamp && ./lamp && chmod -x lamp;;
   * ) echo "Érvénytelen válasz!";;
 esac
 
