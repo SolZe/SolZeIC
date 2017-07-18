85d16a84022b4a0d65d4782dc93379265de764c2
Solinu <szabo.zsolo.laszlo@gmail.com>
2017-07-18 10:41:23 +0200

    Scriptek feltöltve
    Telepítési scriptek feltöltve
     - LAMP (még nem működik)
     - SZIC PE cURL/WGET
     - Wine

---
+++ szic_pe_curl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SolZeIC Preinstallation Environment - cURL
+# Készítette: Solinu
+# Licensz: Apache 2.0
+
+echo "SolZeIC Preinstallation Environment"
+echo
+echo "Szoftverek és betűjeleik listája: https://github.com/Winizsol/telepitesi-scriptek/blob/master/SZOFTVEREK.md"
+read -p "Melyik szoftvert szeretné telepíteni? (betűjel) " choice
+case "$choice" in 
+  a|A ) cd ~/winizsol && curl -o winehq -L https://raw.githubusercontent.com/Winizsol/telepitesi-scriptek/master/wine.sh && chmod +x winehq && ./winehq && chmod -x winehq;;
+  # Ez még nem működik
+  #b|B ) cd ~/winizsol && curl -o lamp -L https://raw.githubusercontent.com/Winizsol/telepitesi-scriptek/master/lamp.sh && chmod +x lamp && ./lamp && chmod -x lamp;;
+  * ) echo "Érvénytelen válasz!";;
+esac
+
+# Források:
+# https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
---
+++ szic_pe_wget.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SolZeIC Preinstallation Environment - WGET
+# Készítette: Solinu
+# Licensz: Apache 2.0
+
+echo "Telepítési Script-ek Előtelepítési Környezet"
+echo
+echo "Szoftverek és betűjeleik listája: https://github.com/Winizsol/telepitesi-scriptek/blob/master/SZOFTVEREK.md"
+read -p "Melyik szoftvert szeretné telepíteni? (betűjel) " choice
+case "$choice" in 
+  a|A ) cd ~/winizsol && wget -O winehq https://raw.githubusercontent.com/Winizsol/telepitesi-scriptek/master/wine.sh && chmod +x winehq && ./winehq && chmod -x winehq;;
+  # Ez még nem működik
+  #b|B ) cd ~/winizsol && wget -O lamp https://raw.githubusercontent.com/Winizsol/telepitesi-scriptek/master/lamp.sh && chmod +x lamp && ./lamp && chmod -x lamp;;
+  * ) echo "Érvénytelen válasz!";;
+esac
+
+# Források:
+# https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
